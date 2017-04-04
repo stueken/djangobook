f0586607aebd1605941b3c994a136049a6c31431
Norbert Stüken <norbert.stueken@nrbrt.com>
2017-04-04 19:12:57 +0200

    feat(mysite): Create first Django powered page: Hello World

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from mysite.views import hello
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
 ]
+
