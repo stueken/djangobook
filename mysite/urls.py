a6c18427cd026b2f989467140f6e05633b804f77
Norbert Stüken <norbert.stueken@nrbrt.com>
2017-04-06 16:58:35 +0200

    feat(mysite): Add views with dynamic datetime content and dynamic urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from mysite.views import hello
+from mysite.views import current_datetime, hello, hours_ahead
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
 
--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,22 @@
-from django.http import HttpResponse
+import datetime
+
+from django.http import Http404, HttpResponse
 
 
 def hello(request):
     return HttpResponse("Hello world")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "It is now %s." % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404p()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "In %s hour(s), it will be %s." % (offset, dt)
+    return HttpResponse(html)
 
