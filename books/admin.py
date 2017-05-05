7b1dc90ce7dbbdaf58b7c5225f6a4ad76e5fe5a7
Norbert Stüken <norbert.stueken@nrbrt.com>
2017-05-05 15:43:26 +0200

    feat(books): Adding models to django admin and changing some field attributes

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, Publisher
+
+
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
