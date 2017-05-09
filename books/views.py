c6b530565c3f728b305abad64a8c3513cf3a45ef
Norbert Stüken <norbert.stueken@nrbrt.com>
2017-05-09 19:07:37 +0200

    feat(books): Improve search form validation

--- books/views.py
+++ books/views.py
@@ -4,12 +4,14 @@ from books.models import Book
 
 
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            error = True
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
         else:
             books = Book.objects.filter(title__icontains=q)
             return render(request, 'search_results.html', {'books': books, 'query': q})
-    return render(request, 'search_form.html', {'error': error})
+    return render(request, 'search_form.html', {'errors': errors})
