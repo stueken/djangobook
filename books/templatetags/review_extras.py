e474c3a55c488fffa6dc445ac239b596f9286f7f
Norbert Stüken <norbert.stueken@nrbrt.com>
2017-05-24 18:48:11 +0200

    feat(books): Create some custom template tags from the ground up

--- books/templatetags/review_extras.py
+++ books/templatetags/review_extras.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 from django import template
 from django.template.defaultfilters import stringfilter
@@ -34,3 +35,103 @@ def initial_letter_filter(text, autoescape=None):
 @register.simple_tag
 def current_time(format_string):
     return datetime.datetime.now().strftime(format_string)
+
+
+@register.tag(name="current_time")
+def do_current_time(parser, token):
+    # This version uses a regular expression to parse tag contents.
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.split_contents(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires a single argument"
+                                           % token.contents.split()[0])
+
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError("%r tag had invalid arguments" % tag_name)
+    format_string, var_name = m.groups()
+
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument should be in quotes" %
+                                           tag_name)
+    return CurrentTimeNode3(format_string[1:-1], var_name)
+
+
+class CurrentTimeNode(template.Node):
+    def __init__(self, format_string):
+        self.format_string = format_string
+
+    def render(self, context):
+        return datetime.datetime.now().strftime(self.format_string)
+
+
+class CurrentTimeNode2(template.Node):
+    def __init__(self, format_string):
+        self.format_string = format_string
+
+    def render(self, context):
+        context['current_time'] = datetime.datetime.now().strftime(self.format_string)
+        return ''
+
+
+class CurrentTimeNode3(template.Node):
+    def __init__(self, format_string, var_name):
+        self.format_string = format_string
+        self.var_name = var_name
+
+    def render(self, context):
+        context[self.var_name] = datetime.datetime.now().strftime(self.format_string)
+        return ''
+
+
+def do_format_time(parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, date_to_be_formatted, format_string = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("%r tag requires exactly two arguments" %
+                                           token.contents.split()[0])
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError("%r tag's argument shouldi be in quotes" %
+                                           tag_name)
+    return FormatTimeNode(date_to_be_formatted, format_string[1:-1])
+
+
+class FormatTimeNode(template.Node):
+    def __init__(self, date_to_be_formatted, format_string):
+        self.date_to_be_formatted = template.Variable(date_to_be_formatted)
+        self.format_string = format_string
+
+    def render(self, context):
+        try:
+            actual_date = self.date_to_be_formatted.resolve(context)
+            return actual_date.strftime(self.format_string)
+        except template.VariableDoesNotExist:
+            return ''
+
+
+def do_comment(parser, token):
+    nodelist = parser.parse(('endcomment',))
+    parser.delete_first_token()
+    return CommentNode()
+
+
+class CommentNode(template.Node):
+    def render(self, context):
+        return ''
+
+
+def do_upper(parser, token):
+    nodelist = parser.parse(('endupper',))
+    parser.delete_first_token()
+    return UpperNode(nodelist)
+
+
+class UpperNode(template.Node):
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        output = self.nodelist.render(context)
+        return output.upper()
