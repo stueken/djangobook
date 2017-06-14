e0982ff84317de5d7bcf6b46fa29b2fee52a1eea
Norbert Stüken <norbert.stueken@nrbrt.com>
2017-06-14 18:22:32 +0200

    test(books): Add test for email functionality

--- books/tests.py
+++ books/tests.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core import mail
 from django.test import TestCase
 from django.utils import timezone
 
@@ -17,3 +18,17 @@ class BookMethodTests(TestCase):
         futuredate = timezone.now().date() + datetime.timedelta(days=5)
         future_pub = Book(publication_date=futuredate)
         self.assertEqual(future_pub.recent_publication(), False)
+
+
+class EmailTest(TestCase):
+    def test_send_email(self):
+        # Send message.
+        mail.send_mail('Subject here', 'Here is the message.',
+                       'from@example.com', ['to@example.com'],
+                       fail_silently=False)
+
+        # Test that one message has been sent.
+        self.assertEqual(len(mail.outbox), 1)
+
+        # Verify that the subject of the first message is correct.
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
